f55c4788324bfb13370f7b5009761adc2faec8a5
malko <msa>
2021-11-02 18:39:57 +0100

    naprawa dynamicProgramming dodane zabezpieczenie przy wczytywaniu

--- PEA_P.cpp
+++ PEA_P.cpp
@@ -4,33 +4,37 @@
 #include <windows.h>
 #include <time.h>
 #include <fstream>
+#include <string>
 #include <utility>
+#include <unordered_map>
 using namespace std;
 
+
 struct podproblem {
-	vector<int> ciag;
 	int liczba;
 	int wartosc;
 };
 
+struct wynikAlgorytmu {
+	vector<int> ciag;
+	int wartosc;
+};
 
-void obliczDwumian(vector<int> arr, int index, vector<int> subarr, vector<vector<int>>& podzbiory) {
-
-	if (index == arr.size()) {
-		int l = subarr.size();
+void obliczDwumian(vector<int> graf, int index, vector<int> podzbior, vector<vector<int>>& podzbiory, int stopien) {
+	if (index == graf.size()) {
+		int l = podzbior.size();
 		if (l != 0) {
-			podzbiory.push_back(subarr);
+			if (podzbior.size() == stopien) {
+				podzbiory.push_back(podzbior);
+			}
 		}
 	}
-	else
-	{
-		obliczDwumian(arr, index + 1, subarr, podzbiory);
-		subarr.push_back(arr[index]);
-		obliczDwumian(arr, index + 1, subarr, podzbiory);
+	else {
+		obliczDwumian(graf, index + 1, podzbior, podzbiory, stopien);
+		podzbior.push_back(graf[index]);
+		obliczDwumian(graf, index + 1, podzbior, podzbiory, stopien);
 	}
 }
-
-
 
 void generacjaGrafu(int rozmiar, vector<vector<int>>& graf) {
 	vector<int> wierzcholki;
@@ -38,12 +42,13 @@ void generacjaGrafu(int rozmiar, vector<vector<int>>& graf) {
 	graf.clear();
 	graf.resize(rozmiar, vector<int>(rozmiar, INT_MAX));
 	for (int i = 0; i < rozmiar; i++) {
-		for (int j = 0; j < rozmiar; j++) {
-			if (i == j) {
-				graf[i][j] = 0;
+		for (int ii = 0; ii <= i; ii++) {
+			if (i == ii) {
+				graf[i][ii] = 0;
 			}
 			else {
-				graf[i][j] = rand() % 150 + 1;
+				graf[i][ii] = rand() % 150 + 1;
+				graf[ii][i] = graf[i][ii];
 			}
 		}
 		wierzcholki[i] = i;
@@ -60,23 +65,27 @@ void generacjaGrafu(int rozmiar, vector<vector<int>>& graf) {
 	graf[wierzcholekB][0] = 0;
 }
 
-void wyswietl(vector<vector<int>> graf) {
-	for (int i = 0; i < graf.size(); i++) {//wywietlenie wierszy
-		for (int j = 0; j < (int)graf.size(); j++) {//wyswtielenie kolumn
-			cout << graf[i][j];
-			if (graf[i][j] > 9) {
-				if (graf[i][j] < 100) {
-					cout << "  ";
-				}
-				else {
-					cout << " ";
-				}
+void wyswietlWiersz(vector<int> graf) {
+	for (int j = 0; j < (int)graf.size(); j++) {//wyswtielenie wierszy
+		cout << graf[j];
+		if (graf[j] > 9) {
+			if (graf[j] < 100) {
+				cout << "  ";
 			}
 			else {
-				cout << "   ";
+				cout << " ";
 			}
 		}
-		cout << endl;
+		else {
+			cout << "   ";
+		}
+	}
+	cout << endl;
+}
+
+void wyswietl(vector<vector<int>> graf) {
+	for (int i = 0; i < graf.size(); i++) {//wywietlenie kolumn
+		wyswietlWiersz(graf[i]);
 	}
 }
 
@@ -86,22 +95,28 @@ int wczytaj(vector<vector<int>>& graf, string nazwa) {
 	fstream dane;
 	string sciezka = "dane\\" + nazwa + ".tsp";
 	dane.open(sciezka, ios::in);
-	dane >> sciezka;
-	dane >> rozmiar;
-	cout << sciezka << endl;
-	graf.resize(rozmiar, vector<int>(rozmiar, INT_MAX));
-	for (int i = 0; i < rozmiar; i++) {
-		for (int ii = 0; ii < rozmiar; ii++) {
-			dane >> graf[i][ii];
-		}
+	if (!dane) {
+		generacjaGrafu(stoi(nazwa), graf);
+		rozmiar = stoi(nazwa);
+		wartoscOptymalna = 0;
+		cout << nazwa << endl;
 	}
-	wyswietl(graf);
-
-	dane >> wartoscOptymalna;
+	else {
+		dane >> sciezka;
+		dane >> rozmiar;
+		cout << sciezka << endl;
+		graf.resize(rozmiar, vector<int>(rozmiar, INT_MAX));
+		for (int i = 0; i < rozmiar; i++) {
+			for (int ii = 0; ii < rozmiar; ii++) {
+				dane >> graf[i][ii];
+			}
+		}
+		dane >> wartoscOptymalna;
+	}
 	return wartoscOptymalna;
 }
 
-std::pair < int, vector<int>> bruteForce(vector<vector<int>> graf) {
+wynikAlgorytmu bruteForce(vector<vector<int>> graf) {
 	int i, minDroga = INT_MAX, droga;
 	vector<int> wierzcholki, najlepszaDroga;
 	wierzcholki.resize(graf.size());
@@ -122,74 +137,119 @@ std::pair < int, vector<int>> bruteForce(vector<vector<int>> graf) {
 
 	} while (next_permutation(wierzcholki.begin(), wierzcholki.end()));
 
-	return std::make_pair(minDroga, najlepszaDroga);
+	wynikAlgorytmu wynik;
+	wynik.ciag = najlepszaDroga;
+	wynik.wartosc = minDroga;
+
+	return wynik;
 }
 
-int dynamicProgramming(vector<vector<int>> graf) {
-	int minDroga = INT_MAX, droga;
-	vector<int> najlepszaDroga;
-	vector<podproblem> podproblemy;
-	vector<int> ciag;
+wynikAlgorytmu dynamicProgramming(vector<vector<int>> graf) {
+	unordered_map<string, podproblem> podproblemy;
 	vector<int> tablica;
+	podproblem najlepszaWartosc, wartosc;
+	unsigned long bits, prev;
 
 	for (int i = 1; i < graf.size(); i++) {
-		podproblem temp;
-		temp.liczba = i;
-		temp.ciag.push_back(i);
-		temp.wartosc = graf[0][i];
-		podproblemy.push_back(temp);
+		wartosc.liczba = i;
+		wartosc.wartosc = graf[0][i];
+		podproblemy[to_string(1 << i) + to_string(i)] = wartosc;
 		tablica.push_back(i);
 	}
 
-	for (int i = 1; i < graf.size() - 1; i++) {
-		vector<vector<int>> podzbior;
+	for (int i = 2; i < graf.size(); i++) {
+		vector<vector<int>> podzbiory;
 		vector<int> dummy;
-		obliczDwumian(tablica, i + 1, dummy, podzbior);
-		for (int ii = 0; ii < podzbior.size(); ii++) {
-			for (int iii = 0; iii < i; iii++) {
-				podproblem temp;
-				temp.ciag = podzbior[ii];
-				temp.liczba = iii;
-				temp.wartosc = graf[iii][ii];
+		obliczDwumian(tablica, 0, dummy, podzbiory, i);
+
+		for (auto podzbior : podzbiory) {
+			bits = 0;
+			for (auto bit : podzbior) {
+				bits |= 1 << bit;
+			}
+
+			for (auto k : podzbior) {
+				prev = bits & ~(1 << k);
+				najlepszaWartosc.liczba = INT_MAX;
+				najlepszaWartosc.wartosc = INT_MAX;
+
+				for (auto m : podzbior) {
+					if (m == k) {
+						continue;
+					}
+					wartosc.wartosc = podproblemy[to_string(prev) + to_string(m)].wartosc + graf[m][k]; //new value
+					wartosc.liczba = m;
+					najlepszaWartosc = (najlepszaWartosc.wartosc < wartosc.wartosc) ? najlepszaWartosc : wartosc; // if best < value than best else value
+				}
+				podproblemy[to_string(bits) + to_string(k)] = najlepszaWartosc;
 			}
 		}
 	}
 
-	return minDroga;
+	bits = (1 << graf.size()) - 2;
+	najlepszaWartosc.liczba = INT_MAX;
+	najlepszaWartosc.wartosc = INT_MAX;
+
+	for (int i = 1; i < graf.size() - 1; i++) {
+
+		wartosc.wartosc = podproblemy[to_string(bits) + to_string(i)].wartosc + graf[i][0];
+		najlepszaWartosc = (najlepszaWartosc.wartosc < wartosc.wartosc) ? najlepszaWartosc : wartosc;
+	}
+
+	wartosc = najlepszaWartosc;
+	tablica.clear();
+	tablica.resize(graf.size() + 1, 0);
+
+	for (int i = 1; i < graf.size(); i++) {
+		tablica[i] = wartosc.liczba;
+		wartosc = podproblemy[to_string(bits) + to_string(tablica[i])];
+		bits = bits & ~(1 << tablica[i]);
+	}
+	wynikAlgorytmu wynik;
+	wynik.ciag = tablica;
+	wynik.wartosc = najlepszaWartosc.wartosc;
+
+	return wynik;
 }
 
+void test(string nazwa) {
+	srand(time(NULL));
+	time_t start, koniec;
+	vector<vector<int>> graf;//graf na którym będziemy pracować
+	long long czas;
+	int wartoscOptymalna = -1;
+	fstream wynik;
+
+
+	wynik.open("wynik.txt", ios::app);//plik do zapisu
+
+	wartoscOptymalna = wczytaj(graf, nazwa);//wczytanie danych z pliku
+
+	wyswietl(graf);
+	start = clock();
+	wynikAlgorytmu wyniki = dynamicProgramming(graf);//wykonanie algorytmu
+	koniec = clock();
+
+	czas = koniec - start;//obliczenie czasu
+	wynik << nazwa << ";" << czas / CLOCKS_PER_SEC << endl;//zapis
+	cout << "czas = " << czas / CLOCKS_PER_SEC << "s" << endl;
+	cout << "wykryta droga = ";
+	wyswietlWiersz(wyniki.ciag);
+	cout << "dlugosc drogi = " << wyniki.wartosc << endl;
+	cout << "optymalna droga = " << wartoscOptymalna << endl;
+	if (wartoscOptymalna == 0) {
+		cout << "blad = " << (1.0 * wyniki.wartosc / 1) * 100 << "%";
+	}
+	else {
+		cout << "blad = " << ((1.0 * wyniki.wartosc / wartoscOptymalna) - 1) * 100 << "%";
+	}
+
+}
 
 
 int main()
 {
-	srand(time(NULL));
-	string nazwa;
-	time_t start, koniec;
-	vector<vector<int>> graf;//graf na którym będziemy pracować
-	long long czas;
-	int wartoscOptymalna;
-	fstream wynik;
+	test("20");
 
-	cin >> nazwa;
-	wynik.open("wynik.txt", ios::app);//plik do zapisu
-
-	//generacjaGrafu(rozmiar, graf);//generacja danych
-	wartoscOptymalna = wczytaj(graf, nazwa);//wczytanie danych z pliku
-
-	start = clock();
-	auto wynik = bruteForce(graf);//wykonanie algorytmu
-	koniec = clock();
-
-
-	auto p = std::make_pair(1, 3.14);
-
-
-
-	czas = koniec - start;//obliczenie czasu
-	wynik << nazwa << " " << czas;//zapis
-	cout << "czas = " << czas / CLOCKS_PER_SEC << "s,\n";
-	cout << "wykryta droga = " << std::get<0>(p);
-	cout << "\noptymalna droga = " << wartoscOptymalna;
-	cout << "\nblad = " << (1.0 * wynik.get() / wartoscOptymalna) - 1;
 	return 0;
 }
